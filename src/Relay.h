7c4ecd610f54aed00dc533352c8f6b7f2a2ae533
ES4A <info@es4a.de>
2021-06-28 10:17:24 +0200

    Create Relay.h

---
+++ src/Relay.h
@@ -0,0 +1,45 @@
+#ifndef Relay_h
+	#define Relay_h
+	
+	extern "C" {
+		
+	}
+
+	#if defined(ARDUINO) && ARDUINO >= 100
+		#include "Arduino.h"
+		#else
+		#include "WProgram.h"
+	#endif 
+	
+	class Relay{
+		public:	
+
+			enum RelayType{
+				none,
+				normal,
+				bistable,
+				powerSurge
+			};
+
+			Relay();
+			Relay(uint8_t activePin);
+            Relay(uint8_t changePin, bool initState);
+			Relay(uint8_t activePin, uint8_t resetPin);
+		
+            void tick();
+            void setState(bool newState);
+            void setActiveLow(bool newActiveLow);
+            void setSwitchTime(uint16_t newTime);
+	
+		private:
+			bool currentState = false;
+			bool newState = false;
+			bool activeLow = false;
+			uint8_t relayActivePin;
+			uint8_t relayResetPin;
+			RelayType Relaytype = none;
+			long startChange;		
+			uint16_t switchTime = 100;
+		
+	};
+#endif //Relay
